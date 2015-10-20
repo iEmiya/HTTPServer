84f1a01e032b23e5f601482fcc7347547cff7678
Emiya <flin@ya.ru>
2015-10-20 20:53:29 +0300

    Исправление ошибки при работе с Chrome/46.0.2490.71

--- Client.cs
+++ Client.cs
@@ -8,166 +8,195 @@ using NLog;
 
 namespace HTTPServer
 {
-    // Класс-обработчик клиента
-    class Client
-    {
-        // Отправка страницы с ошибкой
-        private static void SendError(TcpClient client, HttpStatusCode code)
-        {
-            // Получаем строку вида "200 OK"
-            // HttpStatusCode хранит в себе все статус-коды HTTP/1.1
-            string codeStr = ((int)code).ToString() + " " + code.ToString();
-            // Код простой HTML-странички
-            string html = "<html><body><h1>" + codeStr + "</h1></body></html>";
-            // Необходимые заголовки: ответ сервера, тип и длина содержимого. После двух пустых строк - само содержимое
-            string str = "HTTP/1.1 " + codeStr + "\nContent-type: text/html\nContent-Length:" + html.Length.ToString() + "\n\n" + html;
-            // Приведем строку к виду массива байт
-            byte[] buffer = Encoding.ASCII.GetBytes(str);
-            // Отправим его клиенту
-            client.GetStream().Write(buffer, 0, buffer.Length);
-            // Закроем соединение
-            client.Close();
-        }
+	// Класс-обработчик клиента
+	class Client
+	{
+		// Отправка страницы с ошибкой
+		private static void SendError(TcpClient client, HttpStatusCode code)
+		{
+			// Получаем строку вида "200 OK"
+			// HttpStatusCode хранит в себе все статус-коды HTTP/1.1
+			string codeStr = ((int)code).ToString() + " " + code.ToString();
+			// Код простой HTML-странички
+			string html = "<html><body><h1>" + codeStr + "</h1></body></html>";
+			// Необходимые заголовки: ответ сервера, тип и длина содержимого. После двух пустых строк - само содержимое
+			string str = "HTTP/1.1 " + codeStr + "\nContent-type: text/html\nContent-Length:" + html.Length.ToString() + "\n\n" + html;
+			// Приведем строку к виду массива байт
+			byte[] buffer = Encoding.ASCII.GetBytes(str);
+			// Отправим его клиенту
+			client.GetStream().Write(buffer, 0, buffer.Length);
+			// Закроем соединение
+			client.Close();
+		}
 
-        private readonly Logger _log;
+		private readonly Logger _log;
 
-        // Конструктор класса. Ему нужно передавать принятого клиента от TcpListener
-        public Client(Logger log, TcpClient client)
-        {
-            _log = log;
+		// Конструктор класса. Ему нужно передавать принятого клиента от TcpListener
+		public Client(Logger log, TcpClient client)
+		{
+			_log = log;
+			_log.Trace("Init client");
 
-            // Объявим строку, в которой будет хранится запрос клиента
-            string request = "";
-            // Буфер для хранения принятых от клиента данных
-            byte[] buffer = new byte[1024];
-            // Переменная для хранения количества байт, принятых от клиента
-            int count;
-            // Читаем из потока клиента до тех пор, пока от него поступают данные
-            while ((count = client.GetStream().Read(buffer, 0, buffer.Length)) > 0)
-            {
-                // Преобразуем эти данные в строку и добавим ее к переменной Request
-                request += Encoding.ASCII.GetString(buffer, 0, count);
-                // Запрос должен обрываться последовательностью \r\n\r\n
-                // Либо обрываем прием данных сами, если длина строки Request превышает 4 килобайта
-                // Нам не нужно получать данные из POST-запроса (и т. п.), а обычный запрос
-                // по идее не должен быть больше 4 килобайт
-                if (request.IndexOf("\r\n\r\n", StringComparison.Ordinal) >= 0 || request.Length > 4096)
-                {
-                    break;
-                }
-            }
+			// Объявим строку, в которой будет хранится запрос клиента
+			string request = "";
+			// Буфер для хранения принятых от клиента данных
+			byte[] buffer = new byte[1024];
+			// Переменная для хранения количества байт, принятых от клиента
+			int count;
 
-            // Парсим строку запроса с использованием регулярных выражений
-            // При этом отсекаем все переменные GET-запроса
-            Match reqMatch = Regex.Match(request, @"^\w+\s+([^\s\?]+)[^\s]*\s+HTTP/.*|");
+			_log.Trace("Read request");
+			// Читаем из потока клиента до тех пор, пока от него поступают данные
+			while ((count = client.GetStream().Read(buffer, 0, buffer.Length)) > 0)
+			{
+				// Преобразуем эти данные в строку и добавим ее к переменной Request
+				request += Encoding.ASCII.GetString(buffer, 0, count);
+				// Запрос должен обрываться последовательностью \r\n\r\n
+				// Либо обрываем прием данных сами, если длина строки Request превышает 4 килобайта
+				// Нам не нужно получать данные из POST-запроса (и т. п.), а обычный запрос
+				// по идее не должен быть больше 4 килобайт
+				if (request.IndexOf("\r\n\r\n", StringComparison.Ordinal) >= 0 || request.Length > 4096)
+				{
+					break;
+				}
+			}
 
-            // Если запрос не удался
-            if (reqMatch == Match.Empty)
-            {
-                // Передаем клиенту ошибку 400 - неверный запрос
-                _log.Warn(HttpStatusCode.BadRequest + ": " + request);
-                SendError(client, HttpStatusCode.BadRequest);
-                return;
-            }
+			_log.Trace("Request match");
+			// Парсим строку запроса с использованием регулярных выражений
+			// При этом отсекаем все переменные GET-запроса
+			Match reqMatch = Regex.Match(request, @"^\w+\s+([^\s\?]+)[^\s]*\s+HTTP/.*|");
 
-            // Получаем строку запроса
-            string requestUri = reqMatch.Groups[1].Value;
+			// Если запрос не удался
+			if (reqMatch == Match.Empty)
+			{
+				// Передаем клиенту ошибку 400 - неверный запрос
+				_log.Warn(HttpStatusCode.BadRequest + ": " + request);
+				SendError(client, HttpStatusCode.BadRequest);
+				return;
+			}
 
-            // Приводим ее к изначальному виду, преобразуя экранированные символы
-            // Например, "%20" -> " "
-            requestUri = Uri.UnescapeDataString(requestUri);
+			_log.Trace("Get request Uri");
+			// Получаем строку запроса
+			string requestUri = reqMatch.Groups[1].Value;
 
-            // Если в строке содержится двоеточие, передадим ошибку 400
-            // Это нужно для защиты от URL типа http://example.com/../../file.txt
-            if (requestUri.IndexOf("..", StringComparison.Ordinal) >= 0)
-            {
-                _log.Warn(HttpStatusCode.BadRequest + ": " + request);
-                SendError(client, HttpStatusCode.BadRequest);
-                return;
-            }
 
-            if (string.IsNullOrEmpty(requestUri))
-            {
-                requestUri += "/";
-            }
+			_log.Trace("Unescape request Uri");
+			// Приводим ее к изначальному виду, преобразуя экранированные символы
+			// Например, "%20" -> " "
+			requestUri = Uri.UnescapeDataString(requestUri);
 
-            // Если строка запроса оканчивается на "/", то добавим к ней index.html
-            if (requestUri.EndsWith("/"))
-            {
-                requestUri += "index.html";
-            }
+			// Если в строке содержится двоеточие, передадим ошибку 400
+			// Это нужно для защиты от URL типа http://example.com/../../file.txt
+			if (requestUri.IndexOf("..", StringComparison.Ordinal) >= 0)
+			{
+				_log.Warn(HttpStatusCode.BadRequest + ": " + request);
+				SendError(client, HttpStatusCode.BadRequest);
+				return;
+			}
 
-            string filePath;
-            if (Settings.IsSite)
-            {
-                filePath = Settings.Folder + requestUri;
-            }
-            else
-            {
-                string fileName;
-                try
-                {
-                    fileName = Path.GetFileName(requestUri);
-                }
-                catch (Exception)
-                {
-                    _log.Warn(HttpStatusCode.BadRequest + ": " + request);
-                    SendError(client, HttpStatusCode.BadRequest);
-                    return;
-                }
+			if (string.IsNullOrEmpty(requestUri))
+			{
+				requestUri += "/";
+			}
 
-                if (!Settings.Files.TryGetValue(fileName, out filePath))
-                {
-                    log.Warn(HttpStatusCode.Forbidden + ": " + request);
-                    SendError(client, HttpStatusCode.Forbidden);
-                    return;
-                }
-            }
+			// Если строка запроса оканчивается на "/", то добавим к ней index.html
+			if (requestUri.EndsWith("/"))
+			{
+				requestUri += "index.html";
+			}
 
-            // Если в папке www не существует данного файла, посылаем ошибку 404
-            if (!File.Exists(filePath))
-            {
-                _log.Warn(HttpStatusCode.NotFound + ": " + request + " => " + filePath);
-                SendError(client, HttpStatusCode.NotFound);
-                return;
-            }
+			string filePath;
+			if (Settings.IsSite)
+			{
+				filePath = Settings.Folder + requestUri;
+			}
+			else
+			{
+				string fileName;
+				try
+				{
+					fileName = Path.GetFileName(requestUri);
+				}
+				catch (Exception)
+				{
+					_log.Warn(HttpStatusCode.BadRequest + ": " + request);
+					SendError(client, HttpStatusCode.BadRequest);
+					return;
+				}
 
-            // Тип содержимого
-            string contentType = MimeTypes.GetMimeType(requestUri);
+				if (!Settings.Files.TryGetValue(fileName, out filePath))
+				{
+					log.Warn(HttpStatusCode.Forbidden + ": " + request);
+					SendError(client, HttpStatusCode.Forbidden);
+					return;
+				}
+			}
 
-            // Открываем файл, страхуясь на случай ошибки
-            FileStream fs;
-            try
-            {
-                fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
-            }
-            catch (Exception)
-            {
-                // Если случилась ошибка, посылаем клиенту ошибку 500
-                _log.Warn(HttpStatusCode.InternalServerError + ": " + request + " => " + filePath);
-                SendError(client, HttpStatusCode.InternalServerError);
-                return;
-            }
+			// Если в папке www не существует данного файла, посылаем ошибку 404
+			if (!File.Exists(filePath))
+			{
+				_log.Warn(HttpStatusCode.NotFound + ": " + request + " => " + filePath);
+				SendError(client, HttpStatusCode.NotFound);
+				return;
+			}
 
-            // Посылаем заголовки
-            string headers = "HTTP/1.1 200 OK\nContent-Type: " + contentType + "\nContent-Length: " + fs.Length + "\n\n";
-            byte[] headersBuffer = Encoding.ASCII.GetBytes(headers);
-            client.GetStream().Write(headersBuffer, 0, headersBuffer.Length);
+			_log.Trace("Get Mime type");
+			// Тип содержимого
+			string contentType = MimeTypes.GetMimeType(requestUri);
 
-            // Пока не достигнут конец файла
-            while (fs.Position < fs.Length)
-            {
-                // Читаем данные из файла
-                count = fs.Read(buffer, 0, buffer.Length);
-                // И передаем их клиенту
-                client.GetStream().Write(buffer, 0, count);
-            }
 
-            // Закроем файл и соединение
-            fs.Close();
-            client.Close();
+			_log.Trace("Read file from drive");
+			// Открываем файл, страхуясь на случай ошибки
+			FileStream fs;
+			try
+			{
+				fs = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read);
+			}
+			catch (Exception)
+			{
+				// Если случилась ошибка, посылаем клиенту ошибку 500
+				_log.Warn(HttpStatusCode.InternalServerError + ": " + request + " => " + filePath);
+				SendError(client, HttpStatusCode.InternalServerError);
+				return;
+			}
 
-            _log.Info(HttpStatusCode.OK + ": " + request + " => " + filePath);
-        }
-    }
+			_log.Trace("Create response");
+			try
+			{
+				// Посылаем заголовкиy
+				string headers = "HTTP/1.1 200 OK\nContent-Type: " + contentType + "\nContent-Length: " + fs.Length + "\n\n";
+				byte[] headersBuffer = Encoding.ASCII.GetBytes(headers);
+				client.GetStream().Write(headersBuffer, 0, headersBuffer.Length);
+
+				// Пока не достигнут конец файла
+				while (fs.Position < fs.Length)
+				{
+					// Читаем данные из файла
+					count = fs.Read(buffer, 0, buffer.Length);
+					// И передаем их клиенту
+					client.GetStream().Write(buffer, 0, count);
+				}
+				
+			}
+			catch (Exception ex)
+			{
+				_log.Trace(ex, "Failed to create response");
+			}
+			finally
+			{
+				// Закроем файл и соединение
+				fs.Close();
+				try
+				{
+					client.Close();
+				}
+				catch (Exception ignore)
+				{
+				}
+				
+			}
+			
+
+			_log.Info(HttpStatusCode.OK + ": " + request + " => " + filePath);
+		}
+	}
 }
--- Server.cs
+++ Server.cs
@@ -7,76 +7,88 @@ using NLog;
 
 namespace HTTPServer
 {
-    class Server
-    {
-        internal static readonly Logger Log = LogManager.GetCurrentClassLogger();
+	class Server
+	{
+		internal static readonly Logger Log = LogManager.GetCurrentClassLogger();
 
 
-        private readonly Logger _log;
-        private readonly TcpListener _listener; // Объект, принимающий TCP-клиентов
+		private readonly Logger _log;
+		private readonly TcpListener _listener; // Объект, принимающий TCP-клиентов
 
-        // Запуск сервера
-        public Server(int port)
-        {
-            _log = Log;
+		// Запуск сервера
+		public Server(int port)
+		{
+			_log = Log;
 
-            _listener = new TcpListener(IPAddress.Any, port); // Создаем "слушателя" для указанного порта
-            _listener.Start(); // Запускаем его
+			_listener = new TcpListener(IPAddress.Any, port); // Создаем "слушателя" для указанного порта
+			_listener.Start(); // Запускаем его
 
-            // В бесконечном цикле
-            while (true)
-            {
-                // Принимаем новых клиентов. После того, как клиент был принят, он передается в новый поток (ClientThread)
-                // с использованием пула потоков.
-                ThreadPool.QueueUserWorkItem(new WaitCallback(ClientThread), _listener.AcceptTcpClient());
-            }
-        }
+			// В бесконечном цикле
+			while (true)
+			{
+				// Принимаем новых клиентов. После того, как клиент был принят, он передается в новый поток (ClientThread)
+				// с использованием пула потоков.
+				ThreadPool.QueueUserWorkItem(new WaitCallback(ClientThread), _listener.AcceptTcpClient());
+			}
+		}
 
-        private void ClientThread(Object stateInfo)
-        {
-            // Просто создаем новый экземпляр класса Client и передаем ему приведенный к классу TcpClient объект StateInfo
-            new Client(_log, (TcpClient)stateInfo);
-        }
+		private void ClientThread(Object stateInfo)
+		{
+			TcpClient client = stateInfo as TcpClient;
+			if (client == null)
+			{
+				_log.Fatal("Не верное использование контракта при создании запроса");
+			}
+			try
+			{
+				// Просто создаем новый экземпляр класса Client и передаем ему приведенный к классу TcpClient объект StateInfo
+				new Client(_log, client);
+			}
+			catch (Exception ex)
+			{
+				_log.Fatal(ex, "Внутренняя ошибка сервера. Не удалось обработать полученное сообщение");
+			}
+		}
 
-        // Остановка сервера
-        ~Server()
-        {
-            // Если "слушатель" был создан
-            // Остановим его
-            _listener?.Stop();
-        }
+		// Остановка сервера
+		~Server()
+		{
+			// Если "слушатель" был создан
+			// Остановим его
+			_listener?.Stop();
+		}
 
-        static void Main(string[] args)
-        {
-            Log.Info("Запускаем приложение...");
-            if (!SetCurrentDirectory())
-                return;
-            
-            // Определим нужное максимальное количество потоков
-            // Пусть будет по 4 на каждый процессор
-            int maxThreadsCount = Environment.ProcessorCount * 4;
-            // Установим максимальное количество рабочих потоков
-            ThreadPool.SetMaxThreads(maxThreadsCount, maxThreadsCount);
-            // Установим минимальное количество рабочих потоков
-            ThreadPool.SetMinThreads(2, 2);
+		static void Main(string[] args)
+		{
+			Log.Info("Запускаем приложение...");
+			if (!SetCurrentDirectory())
+				return;
 
-            Settings.Initialization();
+			// Определим нужное максимальное количество потоков
+			// Пусть будет по 4 на каждый процессор
+			int maxThreadsCount = Environment.ProcessorCount * 4;
+			// Установим максимальное количество рабочих потоков
+			ThreadPool.SetMaxThreads(maxThreadsCount, maxThreadsCount);
+			// Установим минимальное количество рабочих потоков
+			ThreadPool.SetMinThreads(2, 2);
 
-            // Создадим новый сервер на порту
-            new Server(Settings.Port);
-        }
+			Settings.Initialization();
 
-        private static bool SetCurrentDirectory()
-        {
-            string location = typeof(Server).Assembly.Location;
-            string currentDirectory = Path.GetDirectoryName(location);
-            if (string.IsNullOrEmpty(currentDirectory))
-            {
-                Log.Error("Не удалось определить каталог для работы");
-                return false;
-            }
-            Environment.CurrentDirectory = currentDirectory;
-            return true;
-        }
-    }
+			// Создадим новый сервер на порту
+			new Server(Settings.Port);
+		}
+
+		private static bool SetCurrentDirectory()
+		{
+			string location = typeof(Server).Assembly.Location;
+			string currentDirectory = Path.GetDirectoryName(location);
+			if (string.IsNullOrEmpty(currentDirectory))
+			{
+				Log.Error("Не удалось определить каталог для работы");
+				return false;
+			}
+			Environment.CurrentDirectory = currentDirectory;
+			return true;
+		}
+	}
 }
